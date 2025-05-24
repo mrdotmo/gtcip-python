847033185f68ce2f64c8e02812e393de26dca6a0
mrdotmo <mrdotmo>
2025-05-23 19:47:19 -0600

    Added another case to 3Sum problem

--- tests/test_p0015_3Sum.py
+++ tests/test_p0015_3Sum.py
@@ -6,3 +6,4 @@ def test_3Sum():
     assert [[-4, 1, 3]] == s.threeSum([1, 2, -4, 3])
     assert [[0, 0, 0]] == s.threeSum([0,0, 0, 0])
     assert [[-1,-1,2],[-1,0,1]] == s.threeSum([-1, 0, 1, 2, -1, -4])
+    assert [[-1,-1,2],[-1,0,1]] == s.threeSum([-1,0,1,2,-1,-4])
