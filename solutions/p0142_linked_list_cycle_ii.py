91150e3cacd32e2e1706da04e357727c892c18cf
mrdotmo <mrdotmo>
2025-05-31 10:00:32 -0600

    Floyd's tortoise and hare find cycle

---
+++ solutions/p0142_linked_list_cycle_ii.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        LeetCode Problem 142: Linked List Cycle Ii
+        https://leetcode.com/problems/linked-list-cycle-ii
+        """
+        slow = head
+        fast = head
+        while True:
+            if slow:
+                slow = slow.next
+            if not fast or not fast.next:
+                return None
+            fast = fast.next.next
+            if slow == fast:
+                break
+        slow = head
+        while slow != fast:
+            if slow:
+                slow = slow.next
+            if fast:
+                fast = fast.next
