0b25c76fc5e5f3d269dc9e5b9463ecae7e95138b
mrdotmo <mrdotmo>
2025-05-31 20:44:01 -0600

    Longest repeated character problem

--- solutions/p0424_longest_repeating_character_replacement.py
+++ solutions/p0424_longest_repeating_character_replacement.py
@@ -1,8 +1,25 @@
+from collections import defaultdict
+
+
 class Solution:
-    def characterReplacement(self):
+    def characterReplacement(self, s: str, k: int) -> int:
         """
         LeetCode Problem 424: Longest Repeating Character Replacement
         https://leetcode.com/problems/longest-repeating-character-replacement/
         """
-        # TODO: Implement solution
-        pass
+        freq_map = defaultdict(int)
+        for i in range(k):
+            freq_map[s[i]] += 1
+        left, right = 0, k
+
+        def is_valid():
+            c = max(freq_map.values())
+            return right - left + 1 - c <= k
+
+        while right < len(s):
+            freq_map[s[right]] += 1
+            if not is_valid():
+                freq_map[s[left]] -= 1
+                left += 1
+            right += 1
+        return right - left
--- tests/test_p0424_longest_repeating_character_replacement.py
+++ tests/test_p0424_longest_repeating_character_replacement.py
@@ -3,7 +3,5 @@ from solutions.p0424_longest_repeating_character_replacement import Solution
 
 def test_Longest_Repeating_Character_Replacement():
     s = Solution()
-
-    # TODO: Implement test cases
-
-    pass
+    assert s.characterReplacement("ABAB", 2) == 4
+    assert s.characterReplacement("AABABBA", 1) == 4
